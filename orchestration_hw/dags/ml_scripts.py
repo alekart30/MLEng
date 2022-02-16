91a2b6c7b49bf0050c26d20b27ebf2c318931900
autoblack <alekart30@gmail.com>
2022-02-16 08:54:23 +0000

    fixup: Format Python code with Black

--- orchestration_hw/dags/ml_scripts.py
+++ orchestration_hw/dags/ml_scripts.py
@@ -10,14 +10,20 @@ import os
 MODELS_PATH = "./models/"
 DATA_PATH = "./data/"
 
+
 def _define_last_model_version():
     models_list = os.listdir(MODELS_PATH)
     if len(models_list) == 0:
         return None
     else:
-        names_without_ext = [os.path.splitext(model_name)[0] for model_name in models_list]
-        last_version = max(model_name.split('_')[-1] for model_name in names_without_ext)
+        names_without_ext = [
+            os.path.splitext(model_name)[0] for model_name in models_list
+        ]
+        last_version = max(
+            model_name.split("_")[-1] for model_name in names_without_ext
+        )
         return int(last_version[1:])
+
 
 def _set_new_model_version():
     last_version = _define_last_model_version()
@@ -25,48 +31,50 @@ def _set_new_model_version():
         return last_version + 1
     else:
         return 1
-        
+
+
 def train_model(**kwargs):
     # read preprocessed data
-    X_train = pd.read_csv(DATA_PATH + 'X_train_transformed.csv')
-    y_train = pd.read_csv(DATA_PATH + 'y_train.csv', index_col=0)
+    X_train = pd.read_csv(DATA_PATH + "X_train_transformed.csv")
+    y_train = pd.read_csv(DATA_PATH + "y_train.csv", index_col=0)
 
     logging.info("Input data successfully read")
 
     # fit model
     model = LogisticRegression(random_state=30, class_weight="balanced")
     model.fit(X_train, y_train)
-    
+
     logging.info("Model successfully fitted")
 
     # save model
     try:
-        version = kwargs['params']['model_version']
+        version = kwargs["params"]["model_version"]
     except:
         version = _set_new_model_version()
 
     with open(MODELS_PATH + "model_v" + str(version) + ".pkl", "wb") as f:
         pickle.dump(model, f)
-    
+
     logging.info("Model successfully saved")
     logging.info(f"Version: {version}")
 
+
 def evaluate_on_train(**kwargs):
     # read preprocessed data
-    X_train = pd.read_csv(DATA_PATH + 'X_train_transformed.csv')
-    y_train = pd.read_csv(DATA_PATH + 'y_train.csv', index_col=0)
+    X_train = pd.read_csv(DATA_PATH + "X_train_transformed.csv")
+    y_train = pd.read_csv(DATA_PATH + "y_train.csv", index_col=0)
 
     logging.info("Input data successfully read")
 
     # load trained model
     try:
-        version = kwargs['params']['model_version']
+        version = kwargs["params"]["model_version"]
     except:
         version = _define_last_model_version()
 
     with open(MODELS_PATH + "model_v" + str(version) + ".pkl", "rb") as f:
         model = pickle.load(f)
-    
+
     logging.info("Fitted model successfully loaded")
     logging.info(f"Version: {version}")
 
@@ -74,22 +82,23 @@ def evaluate_on_train(**kwargs):
     pred_train = model.predict(X_train)
 
     logging.info(f"f2-score: {round(fbeta_score(y_train, pred_train, beta=2), 2)}")
-  
+
+
 def infer_predictions(**kwargs):
     # read preprocessed data
-    X_test = pd.read_csv(DATA_PATH + 'X_test_transformed.csv')
+    X_test = pd.read_csv(DATA_PATH + "X_test_transformed.csv")
 
     logging.info("Input data successfully read")
 
     # load trained model
     try:
-        version = kwargs['params']['model_version']
+        version = kwargs["params"]["model_version"]
     except:
         version = _define_last_model_version()
 
     with open(MODELS_PATH + "model_v" + str(version) + ".pkl", "rb") as f:
         model = pickle.load(f)
-    
+
     logging.info("Fitted model successfully loaded")
     logging.info(f"Version: {version}")
 
@@ -99,13 +108,13 @@ def infer_predictions(**kwargs):
 
     logging.info("Inference is successfully done")
 
+
 def evaluate_on_test():
     # read input data
-    y_test = pd.read_csv(DATA_PATH + 'y_test.csv', index_col=0)
+    y_test = pd.read_csv(DATA_PATH + "y_test.csv", index_col=0)
     y_pred = pd.read_csv(DATA_PATH + "predictions.csv", index_col=0)
 
     logging.info("Input data successfully read")
 
     # evaluate predictions
     logging.info(f"f2-score: {round(fbeta_score(y_test, y_pred, beta=2), 2)}")
-
